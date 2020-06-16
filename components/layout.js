57c29c7640733d103cc2d97cb0abbcd1e564dcad
Ricardo <ricardo.a.nava@gmail.com>
2020-06-15 22:31:06 -0700

    Add burger toggle funtionality

--- components/layout.js
+++ components/layout.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -5,67 +7,74 @@ import Head from 'next/head';
 
 import Footer from './footer';
 
-export default ({ children }) => (
-  <>
-    <Head>
-      <title>Create Next App with Bulma.io</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img
-            src="https://bulma.io/images/bulma-logo.png"
-            width="112"
-            height="28"
-          />
-        </a>
+export default ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Create Next App with Bulma.io</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <img
+              src="https://bulma.io/images/bulma-logo.png"
+              width="112"
+              height="28"
+            />
+          </a>
 
-        <a
-          role="button"
-          className="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
+          <a
+            onClick={() =>
+              document
+                .querySelector('.navbar-menu')
+                .classList.toggle('is-active')
+            }
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarmenu"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <a className="navbar-item">Home</a>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">Home</a>
 
-          <a className="navbar-item">Documentation</a>
+            <a className="navbar-item">Documentation</a>
 
-          <div className="navbar-item has-dropdown is-hoverable">
-            <a className="navbar-link">More</a>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">More</a>
 
-            <div className="navbar-dropdown">
-              <a className="navbar-item">About</a>
-              <a className="navbar-item">Jobs</a>
-              <a className="navbar-item">Contact</a>
-              <hr className="navbar-divider" />
-              <a className="navbar-item">Report an issue</a>
+              <div className="navbar-dropdown">
+                <a className="navbar-item">About</a>
+                <a className="navbar-item">Jobs</a>
+                <a className="navbar-item">Contact</a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">Report an issue</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">Log in</a>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <a className="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a className="button is-light">Log in</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-    {children}
-    <Footer />
-  </>
-);
+      </nav>
+      {children}
+      <Footer />
+    </>
+  );
+};
